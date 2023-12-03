c38aa3ce58965c8b5c50c423859576492b47417f
overdrive1708 <52090022+overdrive1708@users.noreply.github.com>
2023-12-03 18:08:05 +0900

    Add output xml function.

---
+++ src/WindowsDeviceManagerAgent/XmlWriter.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Serialization;
+
+namespace WindowsDeviceManagerAgent
+{
+    /// <summary>
+    /// XMLファイル書き込みクラス
+    /// </summary>
+    internal class XmlWriter
+    {
+        //--------------------------------------------------
+        // 定数(コンフィギュレーション)
+        //--------------------------------------------------
+        /// <summary>
+        /// データベースファイル名
+        /// </summary>
+        private static readonly string _fileName = "WindowsDeviceInfo.xml";
+
+        //--------------------------------------------------
+        // メソッド
+        //--------------------------------------------------
+        /// <summary>
+        /// Windowsデバイス情報書き込み処理
+        /// </summary>
+        /// <param name="writeValue">Windowsデバイス情報</param>
+        public static void WriteWindowsDeviceInfoRecord(WindowsDeviceInfo writeValue)
+        {
+            // シリアライズ&ファイル出力
+            XmlSerializer serializer = new(typeof(WindowsDeviceInfo));
+            using StreamWriter swXml = new(_fileName, false, System.Text.Encoding.UTF8);
+            serializer.Serialize(swXml, writeValue);
+            swXml.Close();
+
+            // 完了メッセージ表示
+            ConsoleWrapper.WriteLine(Resources.Strings.MessageCreateXmlFile);
+        }
+    }
+}
